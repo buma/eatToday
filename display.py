0ee946354ee8575cf82a8f17f928d82ed356181b
Marko Burjek <s0mebody.slo@gmail.com>
2017-06-18 10:36:18 +0200

    Nicer display

--- display.py
+++ display.py
@@ -30,19 +30,31 @@ evening = datetime.datetime.now()
 evening=evening.replace(hour=20, minute=0, second=0)
 
 hours_to_evening = math.ceil((evening-now).total_seconds()/60/60)
+weight = 63
+needed_kcal = 2200
+needed_protein = 1.5*weight
+
+TEMPL="Preostale kal:{:.2f}\nKalorij na 2 uri:{}\nPreostale  beljakovine:{:.2f}"
+TEMPL+="\nBeljakovin na 2 uri:{:.2f}\nKalorije do kosila:{:.2f}"
 
 nutritions = []
 ITEM="{time:^5} {type:^8} {description:50.50} {kalorije:^6} {hidrati:^6} {beljakovine:^7}"
 ITEM+=" {fat:^6}"
 print (ITEM.format(time="time", type="type", description="description",
         kalorije="kcal", hidrati="carb", beljakovine="protein", fat="fat"))
+sumed_lunch = None
 for item in items:
     print ("{}".format(item))
     if item.nutri_info is not None:
         nutritions.append(item.nutri_info)
+        if sumed_lunch is None and item.time.hour >= 14:
+            sumed_lunch = sum(nutritions)
 sumed = (sum(nutritions))
-missing_kcal = 2200-sumed.kcal
-missing_protein = 1.5*63-sumed.protein
+if sumed_lunch is None:
+    sumed_lunch = sumed
+missing_kcal = needed_kcal-sumed.kcal
+missing_protein = needed_protein-sumed.protein
 print ("SUM:"," "*60+"{}".format(sumed))
-print ("Preostale kal:{:.2f}\nKalorij na 2 uri:{}\nPreostale  beljakovine:{:.2f}".format(
-    missing_kcal, missing_kcal/(hours_to_evening/2), missing_protein))
+print (TEMPL.format(missing_kcal, missing_kcal/(hours_to_evening/2),
+    missing_protein, missing_protein/(hours_to_evening/2),
+    needed_kcal/2-sumed_lunch.kcal))
