9d98c851135e39d3ef2941eb05411fe4912ea061
Marko Burjek <s0mebody.slo@gmail.com>
2017-06-30 09:03:34 +0200

    New way of calculating kcal/proteins

--- display.py
+++ display.py
@@ -25,18 +25,56 @@ Session = sessionmaker(bind=engine)
 
 session = Session() 
 
-weight = 63
+weight = 64 #6:54 20.6 7:42 21.6 63
 needed_kcal = 2200
 needed_kcal_lunch = 1300
+#First number are actual calories of lunch
+diff = 0 #  721-500
 #Calculated based on 15:00-7:00/2 per 2 hours
-needed_kcal_2_hours_till_lunch=needed_kcal_lunch/(8/2) 
+needed_kcal_2_hours_till_lunch=(needed_kcal_lunch)/(8/2) 
 lunch_kcal=500
-needed_kcal_2_hours_after_lunch=(needed_kcal-4*needed_kcal_2_hours_till_lunch-lunch_kcal)/2
+if diff > 0:
+    needed_kcal_2_hours_after_lunch=(needed_kcal-4*needed_kcal_2_hours_till_lunch-lunch_kcal)/2-(diff*0.4)/2
+    needed_kcal_2_hours_till_lunch-=(diff*0.6)/4
+else:
+    needed_kcal_2_hours_after_lunch=(needed_kcal-4*needed_kcal_2_hours_till_lunch-lunch_kcal)/2
+
+print ("Needed kcal", needed_kcal, "lunch kcal", lunch_kcal, "tolunch kcal",
+        needed_kcal_2_hours_till_lunch, "after lunch",
+        needed_kcal_2_hours_after_lunch)
+
 
 needed_protein = 1.5*weight
-needed_protein_2_hours_till_lunch=needed_kcal_2_hours_till_lunch/needed_kcal*needed_protein
-lunch_protein=lunch_kcal/needed_kcal*needed_protein
-needed_protein_2_hours_after_lunch=needed_kcal_2_hours_after_lunch/needed_kcal*needed_protein
+needed_protein_lunch= 1300/needed_kcal*needed_protein
+lunch_protein = math.ceil(500/2200*needed_protein)
+diff = 0 # 56-lunch_protein
+needed_protein_2_hours_till_lunch=needed_protein_lunch/(8/2) # needed_kcal_2_hours_till_lunch/needed_kcal*needed_protein
+if diff > 0:
+    needed_protein_2_hours_after_lunch=(needed_protein-4*needed_protein_2_hours_till_lunch-lunch_protein)/2-(diff*0.4)/2
+    needed_protein_2_hours_till_lunch-=(diff*0.6/4)
+else:
+    needed_protein_2_hours_after_lunch=(needed_protein-4*needed_protein_2_hours_till_lunch-lunch_protein)/2
+
+#needed_kcal_2_hours_after_lunch/needed_kcal*needed_protein
+print ("Needed protein", needed_protein, "lunch protein", lunch_protein, "tolunch protein",
+        needed_protein_2_hours_till_lunch, "after lunch",
+        needed_protein_2_hours_after_lunch)
+
+def item_group(item):
+    hour = item.time.hour
+    if hour%2==0:
+        return hour+1
+    else:
+        return hour
+
+#filt_items = filter(lambda x: x.nutri_info is not None and x.time > datetime.datetime.now(), items)
+
+#for key, group in itertools.groupby(filt_items, item_group):
+    #print (key)
+    #for thing in group:
+        #print ("   ",thing)
+
+
 show_part = set()
 def display_part(hour, nutritions):
     show_part.add(hour)
