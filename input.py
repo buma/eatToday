82182fe7bc5a0d163143d228580a456c1d1c4fbc
Marko Burjek <s0mebody.slo@gmail.com>
2017-10-05 17:47:02 +0200

    Moved adding eat to own file

--- input.py
+++ input.py
@@ -1,21 +1,12 @@
 import itertools
-import io
-import traceback
 from PyQt5.QtCore import *                                                                                                                                       
 from PyQt5.QtGui  import *
 from PyQt5.QtWidgets import (
         QMainWindow,
-        QCompleter,
-        QMessageBox,
-        QDialogButtonBox
+        QMessageBox
         )
 
-from PyQt5.QtSql import (
-        QSqlDatabase,
-        QSqlRelation,
-        QSqlRelationalTableModel,
-        QSqlTableModel
-        )
+from PyQt5.QtSql import QSqlDatabase
 
 from ui_input import Ui_MainWindow
 
@@ -26,14 +17,13 @@ from sqlalchemy.exc import DBAPIError
 from database import (
         Item, LocalNutrition, LocalNutritionaliase, FoodNutrition,
         Tag, TagItem, UsdaWeight)
-from util import sort_nutrition_string, calculate_nutrition
 
-from nutritionDialog import NutritionDialog
 import input_chart as i_c
 import input_price as i_p
 import input_best_before as i_bb
 import input_tag as i_t
 import input_nutrition as i_n
+import input_eat as i_e
 
 class MainWindow(QMainWindow, Ui_MainWindow):
 
@@ -45,11 +35,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
 
     def initUI(self):
-        self.buttonBox.accepted.connect(self.add_new)
-        calc_button = self.buttonBox.addButton("Calculate", QDialogButtonBox.ActionRole)
-        calc_button.pressed.connect(self.calculate)
-        self.cb_type.currentTextChanged.connect(self.enable_nutrition)
-        self.cb_usda.stateChanged.connect(self.enable_usda)
         engine = sqlalchemy.create_engine(connectString)
         gourmet_engine = \
                 sqlalchemy.create_engine("sqlite:////home/mabu/.gourmet/recipes_copy.db")
@@ -68,85 +53,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.init_db()
 
-        self.local_nutri_query = self.session \
-                .query(LocalNutritionaliase.ndbno)
-        self.filters = []
 
-        eating_model = QSqlTableModel()
-        eating_model.setTable("eat")
-        eating_model.select()
-
-        nutri_model = QSqlRelationalTableModel()
-        nutri_model.setTable("nutrition")
-        #nutri_model.fieldIndex("ndbno")
-        nutri_model.setRelation(0, QSqlRelation('nutritionaliases', 'ndbno',
-            'ingkey'))
-        nutri_model.setSort(0, Qt.AscendingOrder)
-        nutri_model.select()
-        #print (nutri_model.selectStatement())
-
-        self.lv_keys.setModel(nutri_model)
-        self.update_lv_keys()
-#Do we want all nutritions or just local?
-        #self.cb_bb_item.setModel(model_keys)
-        self.lv_keys.doubleClicked.connect(self.add_key_to_nutrition)
-        self.lv_keys.clicked.connect(self.show_usda)
-
-        self.completer = QCompleter()
-        self.le_description.setCompleter(self.completer)
-        self.completer.setModel(eating_model)
-        self.completer.setCompletionColumn(3)
-
-        self.d_edit.setDateTime(QDateTime.currentDateTime())
-        self.cb_type.addItems(["HRANA", "PIPI", "PIJAČA", "STANJE", "ZDRAVILO",
-            "KAKA"])
-
-        self.cb_tag_select.currentIndexChanged.connect(self.filter_add_nutrition)
-
+        self.init_add_eat()
         self.init_add_nutrition()
         self.init_best_before()
         self.init_price()
         self.init_tag()
         self.init_chart()
 
-    def enable_usda(self, state):
-        if state==Qt.Checked:
-#Remove filter with USDA in it
-            self.filters = list(filter(lambda x: 'USDA' not in x, self.filters))
-        else:
-            self.filters.append("source!='USDA'")
-        self.update_lv_keys()
 
-    def update_lv_keys(self):
-        #print ("FILTER:", self.filters)
-        model_keys = self.lv_keys.model()
-        if self.filters:
-            filter = " AND ".join(self.filters)
-            model_keys.setFilter(filter)
-        else:
-            model_keys.setFilter(None)
-        #print ("Filters:", model_keys.filter(), model_keys.selectStatement())
-
-    def filter_add_nutrition(self, model_index):
-        record = self.cb_tag_select.model().record(model_index) 
-        print (record.field("id").value(), record.field("name").value())
-        id = record.field("id").value()
-#Remove filter with IN in it
-        self.filters = list(filter(lambda x: 'IN' not in x, self.filters))
-        if id == 0:
-            pass
-        else:
-            filtered_local = self.local_nutri_query \
-                    .join(LocalNutrition) \
-                    .join(TagItem) \
-                    .filter(TagItem.tag_id==id)
-            fids = []
-            for fid in filtered_local:
-                fids.append(fid.ndbno)
-            if fids:
-                self.filters.append("nutrition.ndbno IN ("+",".join([str(x) for x in
-                    fids])+")")
-        self.update_lv_keys()
+    def init_add_eat(self):
+        i_e.init_add_eat(self)
 
     def init_add_nutrition(self):
         i_n.init_add_nutrition(self)
@@ -174,26 +91,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def init_best_before(self):
         i_bb.init_best_before(self)
 
-
-
-
-
-
-    def enable_nutrition(self, val):
-        #FIXME: this doesn't filter types currently
-        is_nutrition = val == "HRANA" or val == "PIJAČA"
-        self.le_nutrition.setEnabled(is_nutrition)
-        self.lv_keys.setEnabled(is_nutrition)
-        self.model_data = self.session.query(Item.description.distinct()).filter(Item.type==self.cb_type.currentText())
-        #self.le_description.completer().model().setFilter("type="+self.cb_type.currentText())
-        #print (self.le_description.completer().model().selecStatement())
-        items = (x[0] for x in self.model_data.all())
-        #self.model.setStringList(items)
-
-    def add_key_to_nutrition(self, model_index):
-        if self.le_nutrition.isEnabled():
-            self.le_nutrition.insert(model_index.data())
-
     def _get_selected_ndbno(self, record):
         ingkey = record.field(0).value()
 #Nutritionaliases table
@@ -202,104 +99,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         relation_model.setFilter("ingkey='"+ingkey+"'")
         ndbno = relation_model.data(relation_model.index(0,2))
         return ndbno
-
-
-    def show_usda(self, model_index):
-        if self.le_nutrition.isEnabled():
-#Selected nutrition record
-            record = self.lv_keys.model() \
-                    .record(model_index.row()) 
-            ndbno = self._get_selected_ndbno(record)
-            status = "Units:"
-            units = []
-            nutrition = self.session.query(LocalNutrition.gramdsc1,
-                    LocalNutrition.gramdsc2) \
-                    .filter(LocalNutrition.ndbno==ndbno) \
-                    .one()
-            gramdsc1 = nutrition.gramdsc1
-            gramdsc2 = nutrition.gramdsc2
-            usda_amounts = self.session.query(UsdaWeight.unit) \
-                    .filter(UsdaWeight.ndbno==ndbno) 
-            for usda_amount in usda_amounts:
-                units.append(usda_amount[0])
-            #gramdsc1 = record.field("gramdsc1").value()
-            #gramdsc2 = record.field("gramdsc2").value()
-            #print ("NDBNO:", ndbno)
-            if gramdsc1 is not None:
-                units.append(gramdsc1)
-            if gramdsc2 is not None:
-                units.append(gramdsc2)
-            units.sort()
-            try:
-                if units:
-                    status+=", ".join(units)
-                    self.statusbar.showMessage(status)
-                else:
-                    self.statusbar.clearMessage()
-            except Exception as a:
-                print ("Problem with units:", units)
-                self.statusbar.clearMessage()
-
-
-    #@pyqtSlot("")
-    def add_new(self):
-        model = self.le_description.completer().model()
-        row = model.rowCount()
-        model.insertRow(row)
-        def add_data(idx, data):
-            model.setData(model.createIndex(row, idx),
-                    data, Qt.EditRole)
-        desc = self.le_description.text()
-        nutrition = self.le_nutrition.text() if self.le_nutrition.isEnabled() \
-            and len(self.le_nutrition.text()) > 3 \
-            else None
-        try:
-            if nutrition is not None:
-                nutrition = sort_nutrition_string(nutrition)
-            item = Item(description=desc, nutrition=nutrition,
-                    time=self.d_edit.dateTime().toPyDateTime(),
-                    type=self.cb_type.currentText())
-            add_data(1, str(self.d_edit.dateTime().toPyDateTime()))
-            add_data(2, self.cb_type.currentText())
-            add_data(3, desc)
-            add_data(4, nutrition)
-            print(item)
-            if not model.submitAll():
-                raise Exception(model.lastError().text())
-            #self.session.merge(item)
-            #self.session.commit()
-            self.le_description.setText("")
-            self.le_nutrition.setText("")
-        except Exception as e:
-            iostream = io.StringIO();
-            #Shows error dialog:
-            msg = QMessageBox()
-            msg.setIcon(QMessageBox.Critical)
-            msg.setText("It was not possible to save data")
-            msg.setInformativeText("Error: " + str(e))
-            traceback.print_exc(file=iostream)
-            msg.setDetailedText(iostream.getvalue())
-            msg.exec_()
-            
-            
-    def calculate(self):
-        nutrition = self.le_nutrition.text() if self.le_nutrition.isEnabled() \
-            and len(self.le_nutrition.text()) > 3 \
-            else None
-        try:
-            if nutrition is not None:
-                dlg = NutritionDialog(None,nutrition, self.session)
-                dlg.exec_()
-        except Exception as e:
-            iostream = io.StringIO();
-            #Shows error dialog:
-            msg = QMessageBox()
-            msg.setIcon(QMessageBox.Critical)
-            msg.setText("It was not possible to save data")
-            msg.setInformativeText("Error: " + str(e))
-            traceback.print_exc(file=iostream)
-            msg.setDetailedText(iostream.getvalue())
-            msg.exec_()
 
     def init_chart(self):
         i_c.init_chart(self)
