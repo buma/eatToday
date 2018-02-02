b376ec449f378411d00ef211d19458fd9b4c3c3b
Marko Burjek <s0mebody.slo@gmail.com>
2018-02-02 22:43:28 +0100

    Wrote function that adds food tags
    Based on ingkey, tags and description

--- week_usage_tmp.py
+++ week_usage_tmp.py
@@ -4,7 +4,7 @@ import dateutil.relativedelta
 import sqlalchemy
 import itertools
 from sqlalchemy.sql import func
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, joinedload, subqueryload
 
 from tabulate import tabulate
 
@@ -93,6 +93,7 @@ def fillFoodnutritionTags(session):
             .filter(Tag.id==TagItem.tag_id) \
             .filter(~Tag.name.startswith("Brez")) \
             .filter(~Tag.name.startswith("Za v")) \
+            .filter(~Tag.name.startswith("Sledi")) \
             .filter(~Tag.name.startswith("Vsebuje ")) \
             .group_by(FoodNutritionDetails.fn_id, Tag.id) \
             .order_by(sqlalchemy.desc(FoodNutritionDetails.fn_id)) \
@@ -135,6 +136,176 @@ items_fd = session.query(func.group_concat(FoodTag.name)
     .filter(FoodTagItem.fn_id==FoodNutritionTags.fn_id) \
     .group_by(FoodTagItem.fn_id) \
     .order_by("food_tag_names", FoodNutritionTags.tags)
-print (tabulate(items_fd, headers=["Food tags", "tags"]))
+#print (tabulate(items_fd, headers=["Food tags", "tags"]))
+def make_food_tags(ingkeys, tag_names, descs):
+    food_tags = set()
+    one_tag = [
+            "Sadje",
+            "Čokolada",
+            "Keksi",
+            "Pecivo",
+            "Stročnice",
+            "Sladoled",
+            "Slani prigrizek"
+            ]
+    desc_search = [
+            ("rižot", "rižota"),
+            ("hash", "hash"),
+            ("palačink", "palačinke"),
+            ("šmorn", "šmorn"),
+            ("sarma", "sarma"),
+            ("žganci", "žganci"),
+            ("grostl", "grostl"),
+            ("ričet", "ričet"),
+            ("polpeti", "polpeti"),
+            ("quesadillas", ["quesadillas", "tortilla_stuff"]),
+            ("juha", "juha")
+            ]
+
+    in_desc= lambda q: all([q in x.lower() for x in descs])
+
+    if "Kruh" in tag_names:
+        if "HRENOVKA" in ingkeys or "TEL_HRENOVKA" in ingkeys \
+            or "Meso" in tag_names:
+            food_tags.add("kruh z")
+        else:
+            food_tags.add("kruh")
+            if "Marmelada" in tag_names:
+                food_tags.add("sladki namaz")
+            elif "Namaz" in tag_names:
+                food_tags.add("slan namaz")
+            #tahini, klobase, sir, slani namaz
+    elif "Testenine" in tag_names:
+        #Izlocit treba jusne testenine pa tiste v fizolovih stvareh
+        if not (in_desc("juha") or in_desc("enolončnica")):
+            return set(["testenine"])
+    elif "Mleko" in tag_names and "Keksi" in tag_names:
+        return set(["mleko z keksi"])
+    elif "Zelenjava" in tag_names and "Meso" in tag_names:
+        if not in_desc("grostl"):
+            return set(["meso z zelenjavo"])
+    elif "Zelenjava" in tag_names and "Ribe" in tag_names:
+        return set(["ribe z zelenjavo"])
+    elif in_desc("palačink") and "Krompir" in tag_names:
+        return set(["palačinke", "krompir", "slane_palačinke"])
+    else:
+        for search, tag in desc_search:
+            if in_desc(search):
+                if isinstance(tag, list):
+                    return set(tag)
+                else:
+                    return set([tag])
+        for tag_name in one_tag:
+            if tag_name in tag_names and len(tag_names) == 1:
+                return set([tag_name.lower()])
+    return food_tags
+
+def add_food_tags(session):
+    items = session.query(FoodNutritionDetails) \
+            .options(joinedload(FoodNutritionDetails.nutrition)) \
+            .options(joinedload(FoodNutritionDetails.foodnutrition)) \
+            .order_by(FoodNutritionDetails.fn_id.desc()) 
+            #.limit(32)
+    tag_hier = add_food_hier(session)
+
+    def hierahize_tags(tags):
+        tags_ret = set()
+        for tag in tags:
+            tags_ret.add(tag.name)
+            parents = tag_hier.get(tag.id, [])
+            tags_ret.update(parents)
+        return tags_ret
+    def get_food_tags(session):
+        items = session.query(FoodTag)
+        name_tag = {}
+        for item in items:
+            name_tag[item.name] = item
+        return name_tag
+    food_tags = get_food_tags(session)
+    to_add = set()
+    for fn_id, nutris in itertools.groupby(items, key=lambda x:x.fn_id):
+        foodnutrition = None
+        current_food_tags = []
+        food_tags_set = set()
+        eat_items = []
+        eat_id = None
+        ingkeys = []
+        tags = []
+        for nutri in nutris:
+            if foodnutrition is None:
+                foodnutrition = nutri.foodnutrition
+                current_food_tags = foodnutrition.food_tags
+                food_tags_set = set((x.tag.name for x in current_food_tags))
+                if foodnutrition.items: 
+                    eat_id = foodnutrition.items[0].id
+                eat_items = [x.description for x in foodnutrition.items]
+                print (fn_id,)
+            ingkeys.append(nutri.nutrition.alias.ingkey)
+            tags += filter(lambda tag:not tag.cosmetic, nutri.nutrition.tags) # map(lambda tag:tag.name, nutri.nutrition.tags)
+        if eat_id is None:
+            continue
+        tags = set(tags)
+        print ("TAGS:", tags)
+        tags = hierahize_tags(tags)
+        print ("NUTRITION:", foodnutrition.nutrition)
+        print ("  DESC:", eat_items)
+        print ("  ID:", eat_id)
+        print ("  FOOD_TAGS:", current_food_tags, food_tags_set)
+        print ("  INGKEY:", ingkeys)
+        print ("  TAGS:", tags)
+        new_food_tags = make_food_tags(ingkeys, tags, eat_items)
+        #to_add.update(new_food_tags-food_tags_set)
+        to_add_tags = new_food_tags-food_tags_set
+        to_add.update(to_add_tags)
+        print ("  NFOOD_TAG:", new_food_tags)
+        print ("  NEW TAGS:", to_add_tags)
+        for to_add_tag in to_add_tags:
+            if to_add_tag in food_tags:
+                tag = food_tags[to_add_tag]
+            else:
+                tag = FoodTag(name=to_add_tag)
+                food_tags[to_add_tag] = tag
+            food_tag_item = FoodTagItem(item_id=eat_id)
+            food_tag_item.tag = tag
+            foodnutrition.food_tags.append(food_tag_item)
+        #session.flush()
+            #food_tag_item.foodnutrition=foodnutrition
+
+    #session.commit()
+    print ("TO ADD TAGS", to_add)
+
+
+def add_food_hier(session):
+    parents = {}
+    parent_child = {}
+    for tag_h in session.query(TagHierarchy):
+        if tag_h.parent_tag.cosmetic:
+            continue
+        print (tag_h.parent_tag, " -> ", tag_h.tag)
+        cur_parents = parents.get(tag_h.tag,
+                []) 
+        cur_parents.append(tag_h.parent_tag)
+        parents[tag_h.tag] = cur_parents 
+        if tag_h.tag in parent_child:
+            parents[parent_child[tag_h.tag]].append(tag_h.parent_tag)
+        parent_child[tag_h.parent_tag] = tag_h.tag
+        
+    clean_parents = {}
+    for pid, tag in parents.items():
+        print (pid, tag)
+        clean_parents[pid.id] = [x.name for x in tag]
+    #print (clean_parents)
+    return clean_parents
+
+
+#kasa=session.query(Tag).filter(Tag.name=="Kaša").one()
+#kasa_ndbno=[x.ndbno for x in kasa.nutritions]
+#q=session.query(Item.id, Item.description,).filter(FoodNutritionDetails.ndbno.in_(kasa_ndbno)).filter(Item.calc_nutrition==FoodNutritionDetails.fn_id).group_by(Item.id)
+#for a in q.all():
+    #print(a)
+
+
+#add_food_hier(session)
+add_food_tags(session)
 
 #a=5/0
